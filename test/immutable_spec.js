58f3e45f76f7ef06e7b978d66aa8f8456c9566ea
ramky.gracia <ramky.gracia@gmail.com>
2017-07-15 16:38:33 -0400

    Change implementation addMovie unit test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -5,8 +5,7 @@ describe ('immutability', () => {
 	describe ('A List', () => {
  
 		function addMovie (currentState, movie) {
-			return currentState.set ('movies',currentState.get('movies').
-				push (movie));
+			return currentState.update ('movies', movies => movies.push (movie)));
 		};
 
 		it ('is immutable', () => {
