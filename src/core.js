4b165f26633512f832049c40075084f48a6ff049
ramky.gracia <ramky.gracia@gmail.com>
2017-07-22 20:52:54 -0400

    Added more vote test cases, added getWinners from tally map in core.js,

---
+++ src/core.js
@@ -0,0 +1,32 @@
+import { List, Map } from 'immutable';
+
+export function setEntries (state, entries) {
+	return state.set ('entries', List (entries));
+}
+
+export function getWinners (vote) {
+	if (!vote) return []
+	const [a, b] = vote.get ('pair');
+	const aV = vote.getIn (['tally', a], 0);
+	const bV = vote.getIn (['tally', b], 0);
+	if (aV > bV)
+		return a;
+	else if (bV > aV)
+		return b;
+	else	
+		return [a,b];
+}
+
+export function next (state) {
+	const entries = state.get ('entries').concat (getWinners (state.get('vote')));
+	return state.merge ({
+		vote: Map ({
+			pair: entries.take (2)}
+			),
+			entries: entries.skip (2)
+	});
+}
+
+export function vote (state, entry) {
+	return state.updateIn (['vote','tally', entry], 0, tally => tally + 1);
+}
